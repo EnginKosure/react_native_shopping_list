e904c83746ed5d26b9b29faf3f4aa94031b42254
EnginKosure <engin.kosure@cronos.be>
2020-10-07 02:27:57 +0200

    add icon

--- App.js
+++ App.js
@@ -14,41 +14,22 @@ import {
 import Header from './components/Header';
 import 'react-native-get-random-values';
 import {v4 as uuid} from 'uuid';
-import ListItem from './components/ListItem';
-import AddItem from './components/AddItem';
 import LoginPageTextInput from './components/LoginPageTextInput';
-
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
 const App = () => {
-  const [items, setItems] = useState([
-    {id: uuid(), text: 'Milk'},
-    {id: uuid(), text: 'Eggs'},
-    {id: uuid(), text: 'Bread'},
-    {id: uuid(), text: 'Juice'},
-  ]);
-
-  const deleteItem = (id) => {
-    setItems((prevItems) => {
-      return prevItems.filter((item) => item.id !== id);
-    });
-  };
-
-  const addItem = (text) => {
-    if (!text) {
-      Alert.alert('Error', 'Please enter an item', {text: 'Ok'});
-    } else {
-      setItems((prevItems) => {
-        return [{id: uuid(), text}, ...prevItems];
-      });
-    }
-  };
-
   return (
     <SafeAreaView style={styles.safearea}>
       <KeyboardAvoidingView
         behavior={Platform.OS === 'ios' ? 'padding' : null}
         style={styles.container}>
-        <ScrollView style={{flex: 1}} bounces={false}>
+        <ScrollView style={styles.scrollView} bounces={false}>
           <Header />
+          <Icon
+            style={styles.car}
+            name="cart-arrow-down"
+            size={200}
+            color="gray"
+          />
           <LoginPageTextInput
             placeholder="Enter your e-mail..."
             keyboardType="email-address"
@@ -59,21 +40,6 @@ const App = () => {
             keyboardType="default"
             textContentType="password"
           />
-
-          <AddItem addItem={addItem} />
-          <FlatList
-            data={items}
-            renderItem={({item}) => (
-              <ListItem item={item} deleteItem={deleteItem} />
-            )}
-          />
-          <View style={styles.container}>
-            <Text style={styles.text}>Hello World</Text>
-            <Image
-              source={{uri: 'https://randomuser.me/api/portraits/men/3.jpg'}}
-              style={styles.img}
-            />
-          </View>
         </ScrollView>
       </KeyboardAvoidingView>
     </SafeAreaView>
@@ -81,22 +47,21 @@ const App = () => {
 };
 export default App;
 const styles = StyleSheet.create({
+  scrollView: {
+    flex: 1,
+  },
   safearea: {
     flex: 1,
     backgroundColor: '#80cbc4',
   },
+  car: {
+    textAlign: 'center',
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   text: {
     color: 'darkslateblue',
     fontSize: 25,
-  },
-  img: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
   },
 });
