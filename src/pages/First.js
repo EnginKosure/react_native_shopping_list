7551de77f424e5662c191883114e3412da370ac8
EnginKosure <engin.kosure@cronos.be>
2020-10-20 22:29:13 +0200

    try with more params

--- src/pages/First.js
+++ src/pages/First.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import { SafeAreaView, View, Text, StyleSheet, Button } from 'react-native';
-const number = 55;
+import React, { useState } from 'react';
+import { SafeAreaView, View, Text, StyleSheet, Button, TextInput } from 'react-native';
 const First = ({ navigation }) => {
+    const [userName, setUserName] = useState("");
+    const [number, setNumber] = useState(0);
     return (
         <SafeAreaView>
             <View>
                 <Text>First</Text>
+                <View style={styles.textInput}>
+
+                    <TextInput
+                        value={userName}
+                        onChangeText={text => setUserName(text)}
+                        style={styles.text}
+                    />
+                </View>
+                <View style={styles.textInput}>
+
+                    <TextInput
+                        value={number}
+                        onChangeText={text => setNumber(text)}
+                        style={styles.text}
+                    />
+                </View>
+
                 <Button
                     title="Go to Second Page"
-                    onPress={() => navigation.navigate('SecondPage', { myNumber: number })}
+                    onPress={() => navigation.navigate('SecondPage', { myNumber: number, selectedValue: userName })}
                 />
                 <Button
                     title="Go to Weather App"
@@ -20,4 +38,16 @@ const First = ({ navigation }) => {
 }
 
 export default First
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    textInput: {
+        backgroundColor: '#e0e0e0',
+        padding: 10,
+        margin: 10,
+        borderRadius: 5
+    },
+    text: {
+        margin: 10,
+        borderColor: 'black',
+        borderRadius: 5,
+    }
+})
