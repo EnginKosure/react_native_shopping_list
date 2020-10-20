dfdb1831c6722d389be74260c521c13738e07935
EnginKosure <engin.kosure@cronos.be>
2020-10-20 20:18:10 +0200

    add router

---
+++ src/pages/First.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
+
+const First = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>First</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default First
+const styles = StyleSheet.create({})
---
+++ src/pages/Second.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+
+const Second = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Second</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default Second
+
+const styles = StyleSheet.create({})
